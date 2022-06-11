ed33faa7822f797098eb58901801baefc807531d
kissyouhunter <kissyouhunter@hotmail.com>
2022-06-12 02:39:41 +0800

    Update install_kernel.sh

--- install_kernel.sh
+++ install_kernel.sh
@@ -60,14 +60,14 @@ fi
 
 #安装内核模块
 cd ${new_kernel}
-mkdir -p ${boot_path}/${new_header_path}
+mkdir -p ${new_header_path}
 make modules_install && make install
-make INSTALL_HDR_PATH=${boot_path}/${new_header_path} headers_install
+make INSTALL_HDR_PATH=${new_header_path} headers_install
 cd ${boot_path} && cp -r vmlinuz-${new_kernel} zImage && cp -r uInitrd uInitrd-${new_kernel}
 cd ${dtb_path} && rm -f *
 cp ${root_path}/${new_kernel}/${new_dtb_path}/*.dtb ${dtb_path}
 #打包header
-cd ${root_path} && tar zcvf header-${new_kernel}.tar.gz ${new_header_path}
+cd ${root_path} && tar zcvf header-${new_kernel}.tar.gz header/
 #cp ${root_path}/${new_kernel}/${new_dtb_path}/meson-gxl-s905d-phicomm-n1-thresh.dtb ${dtb_path}
 #打包boot模块
 cd ${boot_path} && tar zcvf boot-${new_kernel}.tar.gz *-${new_kernel} && cp -r boot-${new_kernel}.tar.gz ${root_path}
